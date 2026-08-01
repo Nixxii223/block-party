08ae071eee5d951e994af6fbcae50a49882a48c9
Nicki Barrett <146459728+Nixxii223@users.noreply.github.com>
2026-08-01 15:00:12 -0400

    Update homepage.jsx

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Box, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
-import VenmoCode from '../images/venmo QR.png';
+import VenmoCode from '../images/Venmo QR.png';
 
 export default function Homepage() {
     return (
