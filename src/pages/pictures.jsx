e04726dcf20a778ed34f033db4119159d39c96b4
Nicki Barrett <146459728+Nixxii223@users.noreply.github.com>
2026-08-01 21:43:55 -0400

    Update pictures.jsx

--- src/pages/pictures.jsx
+++ src/pages/pictures.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-
+import { Box, Button } from '@mui/material';
 import './styles.css';
 import { Container } from '@mui/material';
 
