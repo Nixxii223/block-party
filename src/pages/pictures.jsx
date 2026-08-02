5b4d8f26d9a44d045eab016bca49d0a42a6b3be6
Nicki Barrett <146459728+Nixxii223@users.noreply.github.com>
2026-08-02 19:11:40 -0400

    Update pictures.jsx

--- src/pages/pictures.jsx
+++ src/pages/pictures.jsx
@@ -5,6 +5,19 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import { Box, Button } from '@mui/material';
 import './styles.css';
 import { Container } from '@mui/material';
+import Bike from '../src/images/Bikepng.png';
+import CanJam from '../src/images/CanJampng.png';
+import Canopy from '../src/images/Canopypng.png';
+import Chalk from '../src/images/Chalkpng.png';
+import Chat from '../src/images/Chatpng.png';
+import Cooper from '../src/images/Canopypng.png';
+import Food from '../src/images/Foodpng.png';
+import Friends from '../src/images/Friendspng.png';
+import Frisbee from '../src/images/Frisbeepng.png';
+import Girls from '../src/images/Girlspng.png';
+import Party from '../src/images/Partypng.png';
+import JumpRope from '../src/images/JumpRopepng.png';
+import Wagon from '../src/images/Wagonpng.png';
 
 export default function TitlebarImageList() {
     return (
