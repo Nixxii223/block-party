cf1c4ca704c0048c486322c13576494fe985478a
Nicki Barrett <146459728+Nixxii223@users.noreply.github.com>
2026-08-01 20:22:26 -0400

    Update pictures.jsx
    removed pictures

--- src/pages/pictures.jsx
+++ src/pages/pictures.jsx
@@ -2,20 +2,6 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Bike from '..Bike.jpeg';
-import CanJam from '..CanJam.JPEG';
-import Canopy from '..Canopy.JPEG';
-import Chalk from '..Chalk.JPEG';
-import Chat from '..Chat.JPEG';
-import Cooper from '..Canopy.jpeg';
-import Food from '..Food.JPEG';
-import Friends from '..Friends.JPEG';
-import Frisbee from '..Frisbee.JPEG';
-import Girls from '..Girls.JPEG';
-import Group from '..Group.JPEG';
-import JumpRope from '..JumpRope.JPEG';
-import Wagon from '..Wagon.jpeg';
-import Video from '..Video.MOV';
 
 import './styles.css';
 import { Container } from '@mui/material';
@@ -27,27 +13,7 @@ export default function TitlebarImageList() {
                 <h2 className="life-savers-regular">Pictures</h2>
             
                 <h2 className="life-savers-regular">Block Party 2025!</h2>
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
-                <img src={Group} alt="Group" style={{ maxWidth: '200px', width: '100%' }} />
-                
-                <p className="poppins-regular">Food!.</p>
-                <img src={Canopy} alt="Canopy" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Food} alt="Food" style={{ maxWidth: '200px', width: '100%' }} />
-                
-                <p className="poppins-regular">Adult Fun!.</p>
-                <img src={Chat} alt="Chat" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Girls} alt="Girls" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Friends} alt="Friends" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={CanJam} alt="CanJam" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Frisbee} alt="Frisbee" style={{ maxWidth: '200px', width: '100%' }} />
-
-                <p className="poppins-regular">Kid Fun!.</p>
-                <img src={Cooper} alt="Cooper" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={JumpRope} alt="JumpRope" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Wagon} alt="Wagon" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Chalk} alt="Chalk" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Bike} alt="Bike" style={{ maxWidth: '200px', width: '100%' }} />
-                <img src={Video} alt="Video" style={{ maxWidth: '200px', width: '100%' }} />
+              
  
         </Container>
     );
