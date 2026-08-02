ae430dc1b0fd6fb6fdab01fe6f9fc65b7aec1e03
Nicki Barrett <146459728+Nixxii223@users.noreply.github.com>
2026-08-01 21:24:45 -0400

    Update pictures.jsx

--- src/pages/pictures.jsx
+++ src/pages/pictures.jsx
@@ -14,7 +14,28 @@ export default function TitlebarImageList() {
             
                 <h2 className="life-savers-regular">Block Party 2025!</h2>
               
- 
+   <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
+                <img src={Group} alt="Group" style={{ maxWidth: '200px', width: '100%' }} />
+                
+                <p className="poppins-regular">Food!.</p>
+                <img src={Canopy} alt="Canopy" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Food} alt="Food" style={{ maxWidth: '200px', width: '100%' }} />
+                
+                <p className="poppins-regular">Adult Fun!.</p>
+                <img src={Chat} alt="Chat" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Girls} alt="Girls" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Friends} alt="Friends" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={CanJam} alt="CanJam" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Frisbee} alt="Frisbee" style={{ maxWidth: '200px', width: '100%' }} />
+
+                <p className="poppins-regular">Kid Fun!.</p>
+                <img src={Cooper} alt="Cooper" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={JumpRope} alt="JumpRope" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Wagon} alt="Wagon" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Chalk} alt="Chalk" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Bike} alt="Bike" style={{ maxWidth: '200px', width: '100%' }} />
+                <img src={Video} alt="Video" style={{ maxWidth: '200px', width: '100%' }} />
+
         </Container>
     );
 }
